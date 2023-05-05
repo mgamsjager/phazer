e9f8b382ab3ebaa5c01c10ee09e74b2faea8140e
Matthias Gamsjager <matthias.gamsjager@cloudnation.nl>
2023-05-05 08:03:37 +0200

    Basic example

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Phazer the RSS client
 
 
+## Changelog
+
+- v3: Format output
+- v2: Use config gile
+- v1: Add clap 
+- v0: Get simple output from RSS (http://feeds.feedburner.com/tweakers/nieuws)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use rss::Channel;
+use simple_error::SimpleError;
+use std::error::Error;
+use std::io::{BufRead, BufReader};
+
+async fn example_feed() -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get("http://feeds.feedburner.com/tweakers/nieuws")
+        .await?
+        .bytes()
+        .await?;
+    let channel = Channel::read_from(&content[..])?;
+    Ok(channel)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Ok(c) = example_feed().await {
+        for i in c.items {
+            println!(
+                "Date: {0} \t {1} \n\t {2}\n {3}",
+                i.pub_date.unwrap(),
+                i.title.unwrap(),
+                i.description.unwrap(),
+                i.link.unwrap(),
+            )
+        }
+    }
+    Ok(())
 }
